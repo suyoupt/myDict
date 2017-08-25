1eed92f315eb24e1ed617dfade769e3032e5bed3
suyoupt <470361490@qq.com>
2017-08-25 15:23:22 +0800

    初始化项目

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Administrator
+ *
+ * Created on August 25, 2017, 3:20 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
